b9397a12fe9687cae5d3c16fa6e7795b95f9d51a
RidwanAlfarhan <hanleetong@gmail.com>
2025-12-17 22:01:19 +0700

    File Upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,25 +2,13 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5o6MCwEseaP":
+      case "6ChiUZQXWpK":
         Script1();
-        break;
-      case "5tEGNatEDi1":
-        Script2();
         break;
   }
 }
 
 function Script1()
-{
-  var audio = document.getElementById('bgSong');
-audio.src="1.mp3";
-audio.load();
-audio.play();
-audio.volume=0.4;
-}
-
-function Script2()
 {
   var audio = document.getElementById('bgSong');
 audio.src="1.mp3";
